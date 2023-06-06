4514baf83073d8a39482f51ca2d6f6e7b88dd34f
Lucas Job Viana <lucasjobvianna@gmail.com>
2023-06-05 21:50:11 -0300

    Melhorando css do login.

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -30,7 +30,10 @@ class Login extends Component {
         const isButtonHidden = p1Name.length > 2 && p2Name.length > 2 ? false : true;
 
         return (
-            <div id="login">
+            <div className='login-conteiner' >
+                <div className='background-image' ></div>
+
+<div id="login">
                 <form>
                     <label htmlFor="p1-name">Player 1:
                         <input type="text" placeholder='name' id='p1-name' name='p1Name' onChange={this.handleChange} value={p1Name} />
@@ -38,9 +41,11 @@ class Login extends Component {
                     <label htmlFor="p2-name">Player 2:
                         <input type="text" placeholder='name' id='p1-name' name='p2Name' onChange={this.handleChange} value={p2Name} />
                     </label>
-                    <button hidden={isButtonHidden} onClick={this.goToPlay} >Let's Go !!!</button>
+                    <button className='btnBora' hidden={isButtonHidden} onClick={this.goToPlay} >Bora pra batalha !!!</button>
                 </form>
             </div>
+            </div>
+            
         )
     }
 }
